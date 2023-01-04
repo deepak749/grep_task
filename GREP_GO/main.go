ea1f2a74492f7b3246c7af1e5a4ded7702900e11
Deepak Patel <76910920+deepak749@users.noreply.github.com>
2023-01-04 14:37:51 +0530

    Update main.go

--- GREP_GO/main.go
+++ GREP_GO/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"module/grepImplementation/grep"
+	"os"
+	"strings"
 )
 
 func main() {
-	files := []string{"test.txt", "test2.txt", "test3.txt"}
-	pattern := "hello"
-	flag := []string{"-x"}
+	files := []string{}
+	flag := []string{}
+	var pattern string
+	fmt.Print("grep ")
+	reader := bufio.NewReader(os.Stdin)
+	userInput, _ := reader.ReadString('\n')
+
+	fmt.Println(userInput)
+	inputs := strings.Split(userInput, " ")
+	for _, param := range inputs {
+		if strings.Contains(param, "-") {
+			flag = append(flag, param)
+		} else if strings.Contains(param, ".txt") {
+			files = append(files, param)
+		} else {
+			pattern = param
+		}
+	}
+	fmt.Println(files)
 	for _, s := range grep.Search(pattern, flag, files) {
 		fmt.Println(s)
 	}
